1e296ca4db122ee4c157ce7d85aac3f7ef0ea576
irinazhuravleva <irinazhuravleva@yandex.ru>
2020-09-02 21:44:08 +0300

    first commit

--- js/model.js
+++ js/model.js
@@ -7,138 +7,117 @@ if (localStorage.getItem("data") != null) {
     data = JSON.parse(localStorage.getItem('data'));
 } else {
     data = [{
-           }, {
-               id: 0,
-               question: 'невероятный',
-               answer: 'unglaubliche'
-           }, {
-               id: 1,
-               question: 'окружающая среда',
-               answer: 'umfeld'
-           }, {
-               id: 2,
-               question: 'существенный',
-               answer: 'wesentlich'
-
-           }, {
-               id: 3,
-               question: 'научный',
-               answer: 'wissenschaftlich'
-
-           }, {
-               id: 4,
-               question: 'мировоззрение',
-               answer: 'weltbuild'
-           }, {
-               id: 5,
-               question: 'смотреть',
-               answer: 'gucken'
-           }, {
-               id: 6,
-               question: 'идеальный',
-               answer: 'vollkommen'
-           }, {
-               id: 7,
-               question: 'прошлое',
-               answer: 'vergangenheit'
-
-           }, {
-               id: 8,
-                question: 'годный',
-               answer: 'dienlich'
-
-           }, {
-               id: 9,
-               question: 'ставить',
-               answer: 'stellen'
-            }, {
-                id: 10,
-                question: 'копытный',
-                answer: 'hufen'
-            }, {
-                id: 11,
-                question: 'расслаблять',
-                answer: 'entspannen'
-            }, {
-                id: 12,
-                question: 'начинать',
-                answer: 'entspinnen'
-
-            }, {
-                id: 13,
-                question: 'самоуверенный',
-                answer: 'selbstandlich'
-
-            }, {
-                id: 14,
-                question: 'представлять',
-                answer: 'vorstellen'
-            }, {
-                id: 15,
-                question: 'мечтать',
-                answer: 'traum'
-            }, {
-                id: 16,
-                question: 'творить',
-                answer: 'schaffen'
-            }, {
-                id: 17,
-                question: 'отношение',
-                answer: 'beziehung'
-
-            }, {
-                id: 18,
-                question: 'свободный',
-                answer: 'locker'
-
-            }, {
-                id: 19,
-                question: 'грудная клетка',
-                answer: 'brustkorb'
-            }, {
-                id: 20,
-                question: 'луч света',
-                answer: 'lichtstrahl'
-            }, {
-                id: 21,
-                question: 'отчётливый',
-                answer: 'deutlich'
-            }, {
-                id: 22,
-                question: 'благодарность',
-                answer: 'dankbarkeit'
-
-            }, {
-                id: 23,
-                question: 'чувствовать',
-                answer: 'empfinden'
-
-            }, {
-                id: 24,
-                question: 'лечение',
-                answer: 'heilung'
-            }, {
-                id: 25,
-                question: 'шаг',
-                answer: 'umziehen'
-            }, {
-                id: 26,
-                question: 'выходить',
-                answer: 'aussteigen'
-            }, {
-                id: 27,
-                question: 'глубоко',
-                answer: 'tief'
-
-            // }, {
-            //     id: ,
-            //     question: '',
-            //     answer: ''
-
-            // }, {
-            //     id: ,
-            //     question: '',
-            //     answer: ''
+            id: 0,
+            question: 'невероятный',
+            answer: 'unglaubliche'
+        }, {
+            id: 1,
+            question: 'окружающая среда',
+            answer: 'umfeld'
+        }, {
+            id: 2,
+            question: 'существенный',
+            answer: 'wesentlich'
+        }, {
+            id: 3,
+            question: 'научный',
+            answer: 'wissenschaftlich'
+        }, {
+            id: 4,
+            question: 'мировоззрение',
+            answer: 'weltbuild'
+        }, {
+            id: 5,
+            question: 'смотреть',
+            answer: 'gucken'
+        }, {
+            id: 6,
+            question: 'идеальный',
+            answer: 'vollkommen'
+        }, {
+            id: 7,
+            question: 'прошлое',
+            answer: 'vergangenheit'
+        }, {
+            id: 8,
+            question: 'годный',
+            answer: 'dienlich'
+        }, {
+            id: 9,
+            question: 'ставить',
+            answer: 'stellen'
+        }, {
+            id: 10,
+            question: 'копытный',
+            answer: 'hufen'
+        }, {
+            id: 11,
+            question: 'расслаблять',
+            answer: 'entspannen'
+        }, {
+            id: 12,
+            question: 'начинать',
+            answer: 'entspinnen'
+        }, {
+            id: 13,
+            question: 'самоуверенный',
+            answer: 'selbstandlich'
+        }, {
+            id: 14,
+            question: 'представлять',
+            answer: 'vorstellen'
+        }, {
+            id: 15,
+            question: 'мечтать',
+            answer: 'traum'
+        }, {
+            id: 16,
+            question: 'творить',
+            answer: 'schaffen'
+        }, {
+            id: 17,
+            question: 'отношение',
+            answer: 'beziehung'
+        }, {
+            id: 18,
+            question: 'свободный',
+            answer: 'locker'
+        }, {
+            id: 19,
+            question: 'грудная клетка',
+            answer: 'brustkorb'
+        }, {
+            id: 20,
+            question: 'луч света',
+            answer: 'lichtstrahl'
+        }, {
+            id: 21,
+            question: 'отчётливый',
+            answer: 'deutlich'
+        }, {
+            id: 22,
+            question: 'благодарность',
+            answer: 'dankbarkeit'
+        }, {
+            id: 23,
+            question: 'чувствовать',
+            answer: 'empfinden'
+        }, {
+            id: 24,
+            question: 'лечение',
+            answer: 'heilung'
+        }, {
+            id: 25,
+            question: 'шаг',
+            answer: 'umziehen'
+        }, {
+            id: 26,
+            question: 'выходить',
+            answer: 'aussteigen'
+        }, {
+            id: 27,
+            question: 'глубоко',
+            answer: 'tief'
         }];
     localStorage.setItem('data', JSON.stringify(data));
 }
